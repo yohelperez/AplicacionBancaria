7cae118731749dc302de3c8c6235b06c2a94e3fc
Yohel <perezyohel@gmail.com>
2019-09-27 23:48:43 -0500

    version final

--- SucursalBancariaFX/src/co/edu/udea/tecnicas/cuentas/controller/CrearCajaController.java
+++ SucursalBancariaFX/src/co/edu/udea/tecnicas/cuentas/controller/CrearCajaController.java
@@ -2,6 +2,7 @@ package co.edu.udea.tecnicas.cuentas.controller;
 
 import co.edu.udea.tecnicas.cuentas.bsn.CajaBsn;
 import co.edu.udea.tecnicas.cuentas.controller.base.BaseController;
+import co.edu.udea.tecnicas.cuentas.dao.exceptions.DuplicatedKeyException;
 import co.edu.udea.tecnicas.cuentas.model.Caja;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
@@ -23,13 +24,23 @@ public class CrearCajaController extends BaseController{
 		boolean formularioValido=validarCampos(id, contrasenia);
 		if(formularioValido) {
 			Caja caja= new Caja(id,contrasenia);
-			cajaBsn.registrar(caja); 
-			Alert alert= new Alert(Alert.AlertType.INFORMATION);
-			alert.setTitle("REGISTRO DE CAJA");
-			alert.setHeaderText("Cuenta Creada Correctamente!");
-			alert.setContentText("IDENTIFICACION: " + caja.getIdentificacion());
-			alert.showAndWait();
-			limpiarCampos();
+			boolean resultado=cajaBsn.registrar(caja);
+			if(resultado) {
+				Alert alert= new Alert(Alert.AlertType.INFORMATION);
+				alert.setTitle("REGISTRO DE CAJA");
+				alert.setHeaderText("Cuenta Creada Correctamente!");
+				alert.setContentText("IDENTIFICACION: " + caja.getIdentificacion());
+				alert.showAndWait();
+				limpiarCampos();
+			}
+			else {
+				Alert alert= new Alert(Alert.AlertType.ERROR);
+				alert.setTitle("REGISTRO DE CUENTA");
+				alert.setHeaderText("Cuenta Duplicada");
+				alert.setContentText("Diligencie los campos correctamente por favor");
+				alert.showAndWait();
+			}
+			
 		}
 		else {
 			Alert alert= new Alert(Alert.AlertType.ERROR);
